cfadcf178855cd17cbef28b8f97dcb00c55964e5
enzo crivelli <enzo_crivelli97@hotmail.com>
2020-02-29 13:42:26 -0300

    method lambda

--- app/controllers/certificados_controller.rb
+++ app/controllers/certificados_controller.rb
@@ -76,6 +76,10 @@ class CertificadosController < ApplicationController
     end
   end
 
+  def url
+    @url = root_url
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_certificado
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'calendario', to: 'pages#calendario', as: :calendario
-  resources :certificados
+  resources :certificados, except: [:new]
   resources :tecnicos
-  resources :clientes
+  resources :clientes do
+    resources :certificados, only: [:new]
+  end
   get 'proximas_fumigaciones', to: 'pages#proximas_fumigaciones', as: :proximas_fumigaciones
   get 'reportes', to: 'pages#reportes', as: :reportes
   get '/search' => 'pages#search', :as => 'search_page'
