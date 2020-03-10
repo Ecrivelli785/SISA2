ac4da78cfb456ff7b902a443213960f7b0cdf743
mdiaz <mario.a.diaz@live.com.ar>
2020-03-10 08:57:12 -0300

    trabajos sobre reporte de quejas - grilla de resultados de busqueda, etc.

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,7 @@
 Rails.application.routes.draw do
-  #get 'quejas/index'
-  #get 'quejas/show'
-  #get 'quejas/create'
-  #get 'quejas/edit'
-  #get 'quejas/update'
-  #get 'quejas/destroy'
-  
   root to: 'pages#home'
-  get 'calendario', to: 'pages#calendario', as: :calendario
-
-  resources :tecnicos
-  
+ 
+  resources :tecnicos 
   resources :certificados, except: [:new]
   resources :quejas, except: [:new]
 
@@ -19,10 +10,12 @@ Rails.application.routes.draw do
     resources :quejas, only: [:new]
   end
   
+  get 'calendario', to: 'pages#calendario', as: :calendario
   get 'proximas_fumigaciones', to: 'pages#proximas_fumigaciones', as: :proximas_fumigaciones
   get 'reportes', to: 'pages#reportes', as: :reportes
   get '/search' => 'pages#search', :as => 'search_page'
   get '/searchcertificado' => 'pages#searchcertificado', :as => 'searchcertificado_page'
   get '/searchtecnico' => 'pages#searchtecnico', :as => 'searchtecnico_page'
+  get '/searchqueja' => 'pages#searchqueja', :as => 'searchqueja_page'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
